6a570c5f87c43a980c93c3cd56ef720fa625a912
gyubin0302 <rbsks147@daum.net>
2020-09-14 22:55:47 +0900

    dto modify

--- SanProject/src/main/java/com/san/spring/dto/BbsDto.java
+++ SanProject/src/main/java/com/san/spring/dto/BbsDto.java
@@ -3,5 +3,90 @@ package com.san.spring.dto;
 import java.io.Serializable;
 
 public class BbsDto implements Serializable {
+	private int post_number;
+	private String email;
+	private String content;
+	private String location;
+	private int auth_check;
+	private String imgname;
+	private String originfilename;
+	private String wdate;
+
+	public BbsDto() {
+	
+	}
+	
+	public int getPost_Number() {
+		return post_number;
+	}
+
+	public void setPost_Number(int post_number) {
+		this.post_number = post_number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getAuth_check() {
+		return auth_check;
+	}
+
+	public void setAuth_check(int auth_check) {
+		this.auth_check = auth_check;
+	}
+
+	public String getImgname() {
+		return imgname;
+	}
+
+	public void setImgname(String imgname) {
+		this.imgname = imgname;
+	}
+
+	public String getOriginfilename() {
+		return originfilename;
+	}
+
+	public void setOriginfilename(String originfilename) {
+		this.originfilename = originfilename;
+	}
+
+	public String getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+
+	@Override
+	public String toString() {
+		return "BbsDto [post_number=" + post_number + ", email=" + email + ", content=" + content + ", location="
+				+ location + ", auth_check=" + auth_check + ", imgname=" + imgname + ", originfilename="
+				+ originfilename + ", wdate=" + wdate + "]";
+	}
+	
+	
 
 }
--- SanProject/src/main/java/com/san/spring/dto/SanDto.java
+++ SanProject/src/main/java/com/san/spring/dto/SanDto.java
@@ -3,5 +3,81 @@ package com.san.spring.dto;
 import java.io.Serializable;
 
 public class SanDto implements Serializable {
-
+	private int post_number;
+	private String name;
+	private int height;
+	private String course;
+	private String info;
+	private String transport;
+	private String location;
+	private String imgname;
+ 	private String originfilename;
+ 	
+ 	public SanDto() {
+	
+	}
+ 	
+	public int getPost_number() {
+		return post_number;
+	}
+	public void setPost_number(int post_number) {
+		this.post_number = post_number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getTransport() {
+		return transport;
+	}
+	public void setTransport(String transport) {
+		this.transport = transport;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getImgname() {
+		return imgname;
+	}
+	public void setImgname(String imgname) {
+		this.imgname = imgname;
+	}
+	public String getOriginfilename() {
+		return originfilename;
+	}
+	public void setOriginfilename(String originfilename) {
+		this.originfilename = originfilename;
+	}
+	
+	@Override
+	public String toString() {
+		return "SanDto [post_number=" + post_number + ", name=" + name + ", height=" + height + ", course=" + course
+				+ ", info=" + info + ", transport=" + transport + ", location=" + location + ", imgname=" + imgname
+				+ ", originfilename=" + originfilename + "]";
+	}
+ 	
+ 	
 }
